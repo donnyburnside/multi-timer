f80ab8986db9851fbfc404ae86cb43ace642af38
Donny Burnside <donnyburnside1@gmail.com>
2022-01-21 02:26:48 +0000

    Setup navigation

---
+++ src/navigation/index.tsx
@@ -0,0 +1,30 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomeScreen from '../screens/HomeScreen';
+import NotFoundScreen from '../screens/NotFoundScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: 'Home',
+          }}
+        />
+        <Stack.Screen
+          name="NotFound"
+          component={NotFoundScreen}
+          options={{
+            title: 'Not Found',
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
