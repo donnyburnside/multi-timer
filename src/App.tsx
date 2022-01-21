9dddd7d8fe28959df9982cc7a67b9ca2de55a31e
Donny Burnside <donnyburnside1@gmail.com>
2022-01-21 01:30:43 +0000

    Added SafeAreaProvider to the Root component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaProvider>
   );
 }
 
