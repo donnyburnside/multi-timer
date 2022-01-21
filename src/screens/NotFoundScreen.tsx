e746563696bf217d4f05e7b915437c7bab69c906
Donny Burnside <donnyburnside1@gmail.com>
2022-01-21 02:48:49 +0000

    Added 'back' button to NotFound screen

--- src/screens/NotFoundScreen.tsx
+++ src/screens/NotFoundScreen.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Button from '../components/Button';
+
 export default function NotFoundScreen({ navigation, route }) {
   return (
     <View style={styles.screen}>
       <Text>Not Found</Text>
+      <Button
+        text="Go back"
+        onPress={() => navigation.goBack()}
+      />
     </View>
   );
 }
