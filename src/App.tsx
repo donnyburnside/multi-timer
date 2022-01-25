3eb38b467fc2d94cf4222ef712e3688b3893be2e
Donny Burnside <donnyburnside1@gmail.com>
2022-01-25 18:28:03 +0000

    Integrated Redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import * as React from 'react';
+import { Provider } from 'react-redux';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+import { store } from './store';
 import Navigation from './navigation';
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <Navigation />
-      <StatusBar style="auto" />
-    </SafeAreaProvider>
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <Navigation />
+        <StatusBar style="auto" />
+      </SafeAreaProvider>
+    </Provider>
   );
 }
