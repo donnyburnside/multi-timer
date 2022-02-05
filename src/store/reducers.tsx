df88d521e5be7ddb71e14f804b884813e25220c1
Donny Burnside <donnyburnside1@gmail.com>
2022-02-05 01:08:08 +0000

    Added timers reducer

--- src/store/reducers.tsx
+++ src/store/reducers.tsx
@@ -1,5 +1,12 @@
 import { combineReducers } from 'redux';
 
+export const timersReducer = (state = [], action) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+};
+
 export const rootReducer = combineReducers({
-  //
+  timers: timersReducer,
 });
