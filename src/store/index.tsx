523202527c3ada2ecb7c5a24930ef277d95e1e32
Donny Burnside <donnyburnside1@gmail.com>
2022-02-07 12:35:20 +0000

    Added timers reducer

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,7 +1,20 @@
 import { createStore, combineReducers } from 'redux';
 
+const timersReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TIMER':
+      return [...state, {...action.payload}];
+    case 'EDIT_TIMER':
+      return [...state].map((x) => x.id === action.payload.id ? {...x, ...action.payload} : x);
+    case 'DELETE_TIMER':
+      return [...state].filter((x) => x.id !== action.payload);
+    default:
+      return state;
+  }
+};
+
 const rootReducer = combineReducers({
-  //
+  timers: timersReducer,
 });
 
 export const store = createStore(rootReducer);
